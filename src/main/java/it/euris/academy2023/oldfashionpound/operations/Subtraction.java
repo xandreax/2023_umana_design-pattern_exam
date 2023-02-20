ca35af4273e6074ba9e33c195ff7d84ab01884fe
xandreax <xandreaxbora@gmail.com>
2023-02-20 12:45:29 +0100

    Refactoring Subtraction with adding the negative result

--- src/main/java/it/euris/academy2023/oldfashionpound/operations/Subtraction.java
+++ src/main/java/it/euris/academy2023/oldfashionpound/operations/Subtraction.java
@@ -16,6 +16,7 @@ public class Subtraction implements Operation {
     @Override
     public OperationResult calculate() {
         int pences = a1.getPences() - a2.getPences();
+        int pences_rest = pences;
         int pences_surplus = 0;
         if (pences < 0) {
             pences = LIMIT_PENCES + pences;
@@ -23,6 +24,7 @@ public class Subtraction implements Operation {
         }
 
         int shillings = a1.getShillings() - a2.getShillings() - pences_surplus;
+        int shilling_rest = shillings;
         int shillings_surplus = 0;
         if (shillings < 0) {
             shillings = LIMIT_SHILLINGS + shillings;
@@ -30,8 +32,17 @@ public class Subtraction implements Operation {
         }
 
         int pounds = a1.getPounds() - a2.getPounds() - shillings_surplus;
-        
+
+        OperationResult.OperationResultBuilder builder = OperationResult.OperationResultBuilder.builder();
+        if (pounds < 0) {
+            Amount rest = Amount.AmountBuilder.builder().setPounds(pounds).setShillings(shilling_rest).setPences(pences_rest).build();
+            pounds = 0;
+            shillings = 0;
+            pences = 0;
+            builder.setRest(rest);
+        }
         Amount result = Amount.AmountBuilder.builder().setPounds(pounds).setShillings(shillings).setPences(pences).build();
-        return OperationResult.OperationResultBuilder.builder().setResultAmount(result).build();
+
+        return builder.setResultAmount(result).build();
     }
 }
